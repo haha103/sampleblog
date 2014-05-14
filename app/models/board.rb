5f27ed2f1dedf00829a4f855629b5ed90daad4ff
Liu Wei <wliu272@163.com>
2014-05-14 13:55:24 +0800

    test

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,5 +1,6 @@
 class Board < ActiveRecord::Base
 	has_many :posts
+	has_many :comments, through: :posts
 	belongs_to :moderator, class_name: "User", foreign_key: "moderator_id"
 	belongs_to :category
 end
